1109a1f05c468e51f83285e7fdbe31c2ef9f54bc
Chris Thompson <g0kla@arrl.net>
2023-07-20 16:52:39 -0400

    Initial telemetry layouts

--- TelemDefinition/MakeHeadersRTIHU.sh
+++ TelemDefinition/MakeHeadersRTIHU.sh
@@ -1,3 +1,3 @@
 #Leave the RTIHU stuff just in case we switch to it for later LTMs
-python ../GenFlightStruct.py  DownlinkSpecGolf-T.csv -d RTIHU -o
+python GenFlightStruct.py  DownlinkSpecPacsat.csv -d RTIHU -o
 
