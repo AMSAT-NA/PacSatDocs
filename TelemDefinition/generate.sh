92153c7dd5e900878dd07cdc38242d749b060454
Chris Thompson <g0kla@arrl.net>
2026-03-24 13:21:07 -0400

    Lastest layouts

--- TelemDefinition/generate.sh
+++ TelemDefinition/generate.sh
@@ -1,5 +1,5 @@
-python genSpacecraft.py PACSAT rt DownlinkSpecPacsat.csv "header_t commonRtMinmaxWodPayload_t commonRtWodPayload_t realtimeSpecific_t " RTIHU
-python genSpacecraft.py PACSAT wod DownlinkSpecPacsat.csv "header_t commonRtMinmaxWodPayload_t commonRtWodPayload_t wodSpecific_t " RTIHU
-python genSpacecraft.py PACSAT err DownlinkSpecPacsat.csv "header_t rt1Errors_t" RTIHU
-python genSpacecraft.py PACSAT errwod DownlinkSpecPacsat.csv "header_t rt1Errors_t" RTIHU
+python genSpacecraft.py PACSAT rt DownlinkSpecPacsat.csv "header_t commonRtMinmaxWodPayload_t commonRtWodPayload_t realtimeSpecific_t " LEGACY_IHU
+python genSpacecraft.py PACSAT wod DownlinkSpecPacsat.csv "header_t commonRtMinmaxWodPayload_t commonRtWodPayload_t wodSpecific_t " LEGACY_IHU
+python genSpacecraft.py PACSAT err DownlinkSpecPacsat.csv "header_t rt1Errors_t" LEGACY_IHU
+python genSpacecraft.py PACSAT errwod DownlinkSpecPacsat.csv "header_t rt1Errors_t" LEGACY_IHU
 #cp HUSKY_SAT_*.csv ../spacecraft/
